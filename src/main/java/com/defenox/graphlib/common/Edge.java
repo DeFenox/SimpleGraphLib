2ddfe8a66806e42cda49ba5f37fc1f2305d37850
DeFenox <defenox@gmail.com>
2020-07-20 13:24:56 +0300

    Added comment for Edge

--- src/main/java/com/defenox/graphlib/common/Edge.java
+++ src/main/java/com/defenox/graphlib/common/Edge.java
@@ -2,33 +2,64 @@ package com.defenox.graphlib.common;
 
 import java.util.Objects;
 
+/**
+ * An object contains two vertices and weight.
+ * Weight of edge equals 1.0 by default for an unweighted graph.
+ * @param <T> type of vertices.
+ */
 public class Edge<T> {
     private final T startVertex;
     private final T endVertex;
     private final Double weight;
 
+    /**
+     * A constructor with args
+     * @param startVertex first vertex.
+     * @param endVertex second vertex.
+     * @param weight - weight of edge.
+     */
     public Edge(T startVertex, T endVertex, Double weight) {
         this.startVertex = startVertex;
         this.endVertex = endVertex;
         this.weight = weight;
     }
 
+    /**
+     * A constructor with args. Set weight as 1.0.
+     * @param startVertex first vertex.
+     * @param endVertex second vertex.
+     */
     public Edge(T startVertex, T endVertex) {
         this(startVertex, endVertex, 1.0);
     }
 
+    /**
+     *
+     * @return
+     */
     public T getStartVertex() {
         return startVertex;
     }
 
+    /**
+     *
+     * @return
+     */
     public T getEndVertex() {
         return endVertex;
     }
 
+    /**
+     *
+     * @return Double - weight of edge.
+     */
     public Double getWeight() {
         return weight;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -39,11 +70,17 @@ public class Edge<T> {
                 Objects.equals(weight, edge.weight);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(startVertex, endVertex, weight);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "(" + startVertex + ", " + endVertex + " : " + weight + ")";
