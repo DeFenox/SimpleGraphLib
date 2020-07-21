7d15dd1ea9cae44d3b822061286243e0830cd923
DeFenox <defenox@gmail.com>
2020-07-21 11:46:03 +0300

    Added WeightedGraph and its implementation comment

--- src/main/java/com/defenox/graphlib/directed/DirectedWeightedGraph.java
+++ src/main/java/com/defenox/graphlib/directed/DirectedWeightedGraph.java
@@ -8,31 +8,36 @@ import com.defenox.graphlib.search.Search;
 import java.util.ArrayList;
 
 /**
+ * The class extents AbstractGraph class and implements Graph and WeightedGraph interfaces and contains implementation of
+ * Search interface. A field Search initialize by Dijkstra object by default.
  *
- * @param <T>
+ * @param <T> type of vertex.
  */
 public class DirectedWeightedGraph<T> extends AbstractGraph<T> implements WeightedGraph<T> {
 
     /**
-     *
+     * Constructor init search by Dijkstra.
      */
     public DirectedWeightedGraph() {
         super();
     }
 
     /**
+     * Constructor with param.
+     * @param search implementation of Search.
      *
-     * @param search
+     * @see Search
      */
     public DirectedWeightedGraph(Search<T> search) {
         super(search);
     }
 
     /**
+     * Add directed weighted edge.
      *
-     * @param firstVertex
-     * @param secondVertex
-     * @param weight
+     * @param firstVertex first vertex.
+     * @param secondVertex second vertex.
+     * @param weight Double a weight of edge.
      */
     @Override
     public void addEdge(T firstVertex, T secondVertex, Double weight) {
--- src/main/java/com/defenox/graphlib/graph/WeightedGraph.java
+++ src/main/java/com/defenox/graphlib/graph/WeightedGraph.java
@@ -1,15 +1,18 @@
 package com.defenox.graphlib.graph;
 
 /**
+ * The interface provide method addEgde for a weighted graph.
  *
- * @param <T>
+ * @param <T> type of vertex.
  */
 public interface WeightedGraph<T> extends Graph<T> {
+
     /**
+     * Add weighted edge.
      *
-     * @param firstVertex
-     * @param secondVertex
-     * @param weight
+     * @param firstVertex first vertex.
+     * @param secondVertex second vertex.
+     * @param weight Double a weight of edge.
      */
     void addEdge(T firstVertex, T secondVertex, Double weight);
 }
