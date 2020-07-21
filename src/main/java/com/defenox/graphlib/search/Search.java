42d4df428e4ea07a9d1d2bb7a2b81fdfb23fb507
DeFenox <defenox@gmail.com>
2020-07-21 10:57:43 +0300

    Added Search comment

--- src/main/java/com/defenox/graphlib/search/Search.java
+++ src/main/java/com/defenox/graphlib/search/Search.java
@@ -6,16 +6,21 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * The <tt>Search</tt> interface provides method <code>getPath()</code>, which
+ * should return list of edges between two vertices. If a path is not reachable,
+ * it returns empty list.
  *
- * @param <T>
+ * @param <T> type of edge.
  */
 public interface Search<T> {
     /**
      *
-     * @param adjacencyList
-     * @param firstVertex
-     * @param lastVertex
-     * @return
+     *
+     * @param adjacencyList - adjacency  list.
+     * @param fromVertex - start vertex.
+     * @param toVertex - end vertex.
+     * @return - List of edge. If a path is not reachable,
+     *  return empty list.
      */
-    List<Edge<T>> getPath(Map<T, List<Edge<T>>> adjacencyList, T firstVertex, T lastVertex);
+    List<Edge<T>> getPath(Map<T, List<Edge<T>>> adjacencyList, T fromVertex, T toVertex);
 }
